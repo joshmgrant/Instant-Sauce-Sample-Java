32540dddda583b641e51733ca4876b17a9635574
joshgrant <joshua.m.grant@gmail.com>
2018-11-28 13:32:33 -0500

    parallel case for running two TestNG methods at once

--- src/test/java/InstantJavaTestNGTest.java
+++ src/test/java/InstantJavaTestNGTest.java
@@ -73,6 +73,62 @@ public class InstantJavaTestNGTest {
         Assert.assertTrue(driver.findElement(inventoryPageLocator).isDisplayed());
     }
 
+    @Test
+    public void shouldOpenFirefox(Method method) throws MalformedURLException {
+        /** Here we are reading environment variables from your local machine and storing these
+         * values in the variables below. Doing this is a best practice.
+         *
+         * Not sure how to use env variables, follow this -
+         * https://wiki.saucelabs.com/display/DOCS/Best+Practice%3A+Use+Environment+Variables+for+Authentication+Credentials
+         */
+        String sauceUserName = System.getenv("SAUCE_USERNAME");
+        String sauceAccessKey = System.getenv("SAUCE_ACCESS_KEY");
+
+
+        /**
+         * In this section, we will configure our test to run on some specific
+         * browser/os combination in Sauce Labs
+         */
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        //set your user name and access key to run tests in Sauce
+        capabilities.setCapability("username", sauceUserName);
+        //set your sauce labs access key
+        capabilities.setCapability("accessKey", sauceAccessKey);
+        //set browser to Safari
+        capabilities.setCapability("browserName", "Firefox");
+        //set operating system to macOS version 10.13
+        capabilities.setCapability("platform", "Windows 10");
+        //set the browser version to 11.1
+        capabilities.setCapability("version", "59.0");
+        //set your test case name so that it shows up in Sauce Labs
+        capabilities.setCapability("name", method.getName());
+
+        //create a new Remote driver that will allow your test to send
+        //commands to the Sauce Labs grid so that Sauce can execute your tests
+        driver = new RemoteWebDriver(new URL("http://ondemand.saucelabs.com:80/wd/hub"), capabilities);
+
+        //navigate to the url of the Sauce Labs Sample app
+        driver.navigate().to("https://www.saucedemo.com");
+        //Create an instance of a Selenium explicit wait so that we can dynamically wait for an element
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        //wait for the user name field to be visible and store that element into a variable
+        By userNameFieldLocator = By.cssSelector("[type='text']");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(userNameFieldLocator));
+        //type the user name string into the user name field
+        driver.findElement(userNameFieldLocator).sendKeys("standard_user");
+        //type the password into the password field
+        driver.findElement(By.cssSelector("[type='password']")).sendKeys("secret_sauce");
+        //hit Login button
+        driver.findElement(By.cssSelector("[type='submit']")).click();
+
+        //Synchronize on the next page and make sure it loads
+
+        By inventoryPageLocator = By.id("inventory_container");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(inventoryPageLocator));
+        //Assert that the inventory page displayed appropriately
+        Assert.assertTrue(driver.findElement(inventoryPageLocator).isDisplayed());
+    }
+
 
     /**
      *Below we are performing 2 critical actions. Quitting the driver and passing
